1e12f939b71be479e02b59844fbe402904eefc07
Michael Peterson <mtpeterson1948@gmail.com>
2023-08-09 04:52:14 -0600

    Added additional error checking
    Added setsAreAvailable()
    Added results checking

--- Sources/AutoEquip.lua
+++ Sources/AutoEquip.lua
@@ -19,6 +19,31 @@ local EMPTY_STR		= dbg.EMPTY_STR
 local SUCCESS		= dbg.SUCCESS
 local FAILURE		= dbg.FAILURE
 
+function equip:setsAreAvailable()
+	local isValid = true
+	local reason = nil
+
+	if UnitLevel("Player") < 10 then
+		reason = sprintf("ERROR: %s must be level 10 or above to use the equipment manager.", UnitName( "Player"))
+		isValid = false
+		return isValid, reason
+	end
+	-- check whether one or more equipment sets are useable
+	if not C_EquipmentSet.CanUseEquipmentSets() then
+		reason = sprintf("ERROR: No usable equipment sets are available. This error often arises\n because an equipment set is missing one or more items.")
+		isValid = false
+		return isValid, reason
+	end
+
+	local setIDs = C_EquipmentSet.GetEquipmentSetIDs()
+
+	if setIDs == nil or #setIDs == 0 then
+		reason = sprintf("ERROR: %s has not yet defined any equipment sets.", UnitName("Player"))
+		isValid = false
+		return isValid, reason
+	end
+	return isValid, reason
+end
 local function enumSetNames()
 	local setIDs = C_EquipmentSet.GetEquipmentSetIDs()
 	local ss = nil
@@ -30,7 +55,7 @@ local function enumSetNames()
 	return setNames
 end
 local function getSetNameByID( setId )
-
+	local result = {SUCCESS, EMPTY_STR, EMPTY_STR}
 	local setName = nil
 
 	local setIDs = C_EquipmentSet.GetEquipmentSetIDs()
@@ -40,19 +65,28 @@ local function getSetNameByID( setId )
 			return setName
 		end
 	end
-	return setName
+	if setName == nil then
+		result = equipdbg:setResult(sprintf("Equipment set %d not found", setId ), debugstack(2) )
+	end
+
+	return setName, result
 end
 local function getSetIdByName( setName )
+	local result = {SUCCESS, EMPTY_STR, EMPTY_STR}
 	local setId = nil
 
-	local setIDs= C_EquipmentSet.GetEquipmentSetIDs()
+	local setIDs = C_EquipmentSet.GetEquipmentSetIDs()
 	for i = 1, #setIDs do
 		local name, _, id, isEquipped = C_EquipmentSet.GetEquipmentSetInfo( setIDs[i] )
 		if name == setName then 
 			setId = id
 		end
 	end
-	return setId
+	if setId == nil then
+		result = equipdbg:setResult(sprintf("%s equipment set not found", setName ), debugstack(2) )
+	end
+
+	return setId, result
 end
 local function setIsEquipped( setId )
 	local equipped = false
@@ -67,7 +101,9 @@ local function setIsEquipped( setId )
 	return equipped
 end
 local function getEquippedSet()
-
+	if not C_EquipmentSet.CanUseEquipmentSets() then
+		return nil, nil, nil
+	end
 	local setIDs = C_EquipmentSet.GetEquipmentSetIDs()
 	for i = 1, #setIDs do
 		local setName, _, setId, isEquipped = C_EquipmentSet.GetEquipmentSetInfo( setIDs[i] )
@@ -83,7 +119,7 @@ local function equipSetByID( setId )
 
 	local setName = getSetNameByID( setId )
 	if setName == nil then
-		local errMsg = sprintf("%s %d Not found. Set Could not be equipped!\n", equipdbg:prefix(), setId )
+		local errMsg = sprintf("%d Not found. Set Could not be equipped!\n", setId )
 		result = equipdbg:setResult( errMsg, equipdbg:simpleStackTrace() )
 		if not result[1] then msgf:postResult( result ) return end
 	end
@@ -93,26 +129,26 @@ local function equipSetByName( setName )
 	local result = { SUCCESS, EMPTY_STR , EMPTY_STR }
 	local setId, result = getSetIdByName( setName )
 	if setId == nil then
-		local errMsg = sprintf("%s %s Not found. Could not be equipped!\n", equipdbg:prefix(), setName )
+		local errMsg = sprintf("%s Not found. Could not be equipped!\n", setName )
 		result = equipdbg:setResult( errMsg, equipdbg:simpleStackTrace() )
 		if not result[1] then msgf:postResult( result ) return end
 	end
 
 	local wasEquipped = C_EquipmentSet.UseEquipmentSet( setId ) 
 	if not wasEquipped then
-		local errMsg = sprintf("%s %s could not be equipped.", equipdbg:prefix(), setName )
+		local errMsg = sprintf("%s could not be equipped.", setName )
 		result = equipdbg:setResult( errMsg, equipdbg:simpleStackTrace() )
 		if not result[1] then msgf:postResult( result ) return end
 	end
 end 
 function equip:setRestXpSet( inputRestingSetName ) -- Set only via the options menu
+	local result = {SUCCESS, EMPTY_STR, EMPTY_STR}
 
-	local restingSetId 	= getSetIdByName( inputRestingSetName )
+	local restingSetId, result 	= getSetIdByName( inputRestingSetName )
+	if not result[1] then return result end
+
 	local _, equippedSetId = getEquippedSet()
 	autoEquipSavedVars 	= { restingSetId, equippedSetId }
-
-	print( dbg:prefix(), "resting Id", restingSetId, "saved Id", equippedSetId )
-
 
 	-- If player is already in a resting zone,
 	-- equip the rest XP set.
@@ -120,7 +156,7 @@ function equip:setRestXpSet( inputRestingSetName ) -- Set only via the options m
 		C_EquipmentSet.UseEquipmentSet( autoEquipSavedVars[1] )
 	end	
 
-	C_UI.Reload()
+	return result
 end
 
 local eventFrame = CreateFrame("Frame" )
@@ -143,24 +179,41 @@ function( self, event, ... )
 
 	if event == "PLAYER_UPDATE_RESTING" then
 		-- Return if saved vars have not yet been set.
-		if autoEquipSavedVars == nil then return end
+		if autoEquipSavedVars ~= nil then
+			if autoEquipSavedVars[1] == nil then return end
+			if autoEquipSavedVars[2] == nil then return end
+		else
+			return
+		end
+
 		local msg = nil
 
 		local restingSetName 	= getSetNameByID( autoEquipSavedVars[1] )
 		local savedSetName 		= getSetNameByID( autoEquipSavedVars[2])
 
+		local setWasEquipped = nil
 		if IsResting() then
 			-- Player has entered, or already is in a resting zone. If not already set, 
 			-- equip the specified resting XP set.
-			C_EquipmentSet.UseEquipmentSet( autoEquipSavedVars[1] )
+			setWasEquipped = C_EquipmentSet.UseEquipmentSet( autoEquipSavedVars[1] )
+			if not setWasEquipped then
+				local result = equipdbg:setResult( "Failed to equip specified set.",debugstack(2))
+				msfg:postResult( result )
+				return
+			end
 			msg = sprintf("ENTERED Rest Area: switched to %s (Id = %d).", restingSetName, autoEquipSavedVars[1] )
 		else
 			-- Player has left the resting zone or is not in one.
-			C_EquipmentSet.UseEquipmentSet( autoEquipSavedVars[2] )
+			setWasEquipped = C_EquipmentSet.UseEquipmentSet( autoEquipSavedVars[2] )
+			if not setWasEquipped then
+				local result = equipdbg:setResult( "Failed to equip specified set.", debugstack(2))
+				msfg:postResult( result )
+				return
+			end
 			msg = sprintf("LEFT Rest area: switched to %s (Id = %d).", savedSetName, autoEquipSavedVars[2] )
 		end
-		UIErrorsFrame:AddMessage( msg, 0.0, 0.5, 0.5 )
-		DEFAULT_CHAT_FRAME:AddMessage( msg, 0.0, 0.5, 0.5 )
+		UIErrorsFrame:AddMessage( msg, 0.0, 1.0, 0.0 )
+		DEFAULT_CHAT_FRAME:AddMessage( msg, 0.0, 1.0, 0.0 )
 	end
 	if event == "ADDON_LOADED" and arg[1] == L["ADDON_NAME"] then
 
@@ -191,5 +244,7 @@ function equip:set( setName ) -- param may be a name (string) or Id (number)
 	if not result[1] then msgf:postResult( result ) end
 end
 
-local fileName = "AutoEquip.lua"
-DEFAULT_CHAT_FRAME:AddMessage( sprintf("%s loaded", fileName), 1.0, 1.0, 0.0 )
+if dbg:debuggingIsEnabled() then
+	local fileName = "AutoEquip.lua"
+	DEFAULT_CHAT_FRAME:AddMessage( sprintf("%s loaded", fileName), 1.0, 1.0, 0.0 )
+end
