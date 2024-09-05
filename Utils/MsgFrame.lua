3b100734ac10b08651aa3b3bc38095f87ce3a13c
Michael Peterson <mtpeterson1948@gmail.com>
2024-09-04 19:07:41 -0600

    Update MsgFrame.lua
    Added two new frames: infoMsgFrame, errorMsgFrame, and helpMsgFrame

--- Utils/MsgFrame.lua
+++ Utils/MsgFrame.lua
@@ -8,23 +8,27 @@ local ADDON_NAME, AutoEquip = ...
 AutoEquip = AutoEquip or {}
 AutoEquip.MsgFrame = {}
 
-local L = AutoEquip.Locales
-local mf = AutoEquip.MsgFrame
+local mf 		= AutoEquip.MsgFrame
 local dbg       = AutoEquip.Debug	-- use for error reporting services
-local frames = AutoEquip.Frames
+local frames 	= AutoEquip.Frames
+local L = AutoEquip.Locales
 
-local FAILURE = false
 
-local infoMsgFrame = frames:createMsgFrame( L["USER_INFO_FRAME"] )
+local FAILURE 		= false
+local infoMsgFrame	= frames:createMsgFrame( L["TITLE_INFO_MSG_FRAME"] )
+local errorMsgFrame = frames:createMsgFrame( L["TITLE_ERROR_MSG_FRAME"] )
+local helpMsgFrame 	= frames:createHelpFrame( L["TITLE_HELP_MSG_FRAME"] )
 
-local errorMsgFrame = frames:createErrorMsgFrame( L["USER_ERROR_FRAME"] )
+infoMsgFrame:Hide()
+errorMsgFrame:Hide()
+helpMsgFrame:Hide()
 
 function mf:postMsg( msg )
+	msg = string.format("%s\n", msg )
 	infoMsgFrame.Text:Insert( msg )
 	infoMsgFrame:Show()
 end
 function mf:postResult( result )
-
 	if result[1] ~= FAILURE then return end
 	
 	local resultString = string.format("%s %s\n", result[3], result[2] )
@@ -32,8 +36,13 @@ function mf:postResult( result )
 	errorMsgFrame:Show()
 end
 
+function mf:helpFrame()
+	helpMsgFrame:Show()
+end
+
+
 local fileName = "MsgFrame.lua"
 if dbg:debuggingIsEnabled() then
-    DEFAULT_CHAT_FRAME:AddMessage( string.format("%s loaded.", fileName ), 1,1,1 )
+    DEFAULT_CHAT_FRAME:AddMessage( string.format("[%s] %s loaded.", ADDON_NAME, fileName ), 1,1,1 )
 end
 
